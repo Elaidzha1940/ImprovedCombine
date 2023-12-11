71d13a32d48a34f3ddd95793c2a93f5b87411c77
Elaidzha1940 <eli080293@gmail.com>
2023-12-11 16:19:53 +0300

    ImprovedCombine

---
+++ ImprovedCombine/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: ImprovedCombine
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 11.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+         
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImprovedCombine/ImprovedCombineApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: ImprovedCombine
+//  File: ImprovedCombineApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 11.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct ImprovedCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
